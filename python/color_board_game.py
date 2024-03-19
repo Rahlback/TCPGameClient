ad2795cfba7aa8a0140a7f92fdf0b52703861301
Rahlback <14808903+Rahlback@users.noreply.github.com>
2024-03-19 21:02:30 +0100

    Upload TCPGameClient

---
+++ python/color_board_game.py
@@ -0,0 +1,154 @@
+from tcp_client import TCPClient
+from time import sleep
+from math import ceil
+from random import randint
+
+server_ip = "192.168.50.162"
+server_port = 9080
+
+user_id = 15
+name = "Rasmus"
+
+class BoardGame:
+    class Board:
+        def __init__(self, board_map, player_positions) -> None:
+            self.map = board_map # [ [], [], [], ...]
+            self.positions = {} # Name: [x, y]
+                
+
+    def __init__(self, user_id=user_id, name=name) -> None:
+        self.state = 0
+        self.boards = []
+
+        self.client = TCPClient(user_id)
+        user_name = name + "-" + str(user_id)
+        self.client.setup_connection(user_id, user_name, server_ip, server_port)
+
+        # Wait until board game starts?
+        # self.client.get_message()
+    
+
+    def deserialize_boards(self, serialized_boards) -> list:
+        """Deserializes the boards received from the game server.
+        The serialized data is of the form:
+            4 bytes = number of boards
+                block of board [
+                    2 bytes = width, gives the number of bits needed for each row
+                        'bytes_per_row' = ceil(width / 8)
+                    2 bytes = height, number of blocks that will follow
+                    
+                    block of row [
+                        bytes_per_row bytes = row_data, data in bytes. 
+                                             0x0ff0 -> [0x0f, 0xf0]. (Needs to be reversed during assembly)
+                    ]
+                    
+            ]
+        """
+        boards = []
+        number_of_boards = 0
+        for x in range(0, 4):
+            number_of_boards <<= 8
+            number_of_boards += serialized_boards[x]
+        print("Number of boards: ", number_of_boards)
+
+        byte_index = 4
+        for board_i in range(0, number_of_boards):
+            board = []
+            width = serialized_boards[byte_index] << 8 
+            width += serialized_boards[byte_index + 1]
+            bytes_per_row = ceil(width / 8)
+            print("Bytes per row: ", bytes_per_row)
+
+            byte_index += 2
+            height = serialized_boards[byte_index] << 8
+            height += serialized_boards[byte_index + 1]
+            print("Height: ", height)
+
+            byte_index += 2
+
+            for row in range(0, height):
+                board_row_data = list(serialized_boards[byte_index:byte_index+bytes_per_row])
+                board_row_data = board_row_data.reverse()
+                board_row = []
+
+                row_data = 0
+                for byte in range(bytes_per_row):
+                    row_data += (serialized_boards[byte_index] << (8 * byte))
+                    byte_index += 1
+        
+                for col in range(0, width):
+                    # print(col)
+                    if ((row_data & (1 << col)) > 0):
+                        board_row.append("#")
+                    else:
+                        board_row.append(" ")
+                board.append(board_row)
+
+            boards.append(board)
+        for b in boards:
+            for row in b:
+                print(row)
+            print("------")
+        return boards
+        
+
+    def setup_game(self):
+        data_buffer = self.client.get_message()
+        print("Trying to setup game")
+        if data_buffer.decode() == "GAME_STARTING":
+            print("Start game received. Waiting for boards")
+            board_buffer = self.client.get_message()
+            self.deserialize_boards(board_buffer)
+            
+            print("Waiting for player positions")
+            player_positions_buffer = list(self.client.get_message())
+            print(len(player_positions_buffer), player_positions_buffer)
+
+            print("Waiting for my player number")
+            player_number = list(self.client.get_message())
+            sleep(0.001)
+            print(len(player_number), player_number)
+            self.state = 1
+            
+
+    def tick(self):
+        data_buffer = self.client.get_message()
+        message = data_buffer.decode()
+        if "GAME_OVER" in message:
+            print("All games are now over. Exiting program")
+        elif "SEND_MOVES" in message:
+            print("Send moves received. Sending moves")
+            possible_moves = ['R', 'L', 'U', 'D']
+            self.client.send(possible_moves[randint(0, 3)] + possible_moves[randint(0, 3)] + possible_moves[randint(0, 3)])
+        else: # New board state (Just the updates positions of players)
+            print("Waiting for updated player positions")
+            # player_positions_buffer = list(self.client.get_message())
+            # print(len(player_positions_buffer), player_positions_buffer)
+
+
+        # elif data_buffer == bytearray("GAME_STARTING", "ASCII"):
+        
+    
+
+
+
+def main(num_of_players=1):
+    boards = []
+    id = 15
+    for x in range(0,num_of_players):
+        id += 100
+        boards.append(BoardGame(user_id=id))
+        
+
+    while True:
+        sleep(0.1)
+        for board in boards:
+            if board.state == 0:
+                board.setup_game()
+                print("\n-----------------\n")
+            else:
+                board.tick()
+        
+
+if __name__ == "__main__":
+    main(4)
---
+++ python/tcp_client.py
@@ -0,0 +1,134 @@
+import socket
+import time
+import datetime
+
+class TCPClient:
+
+    def __init__(self, user_id=0):
+        # self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.user_id = user_id
+        self.end_of_message = bytearray([0])
+    
+    def connect(self, server_ip: str, server_port: int, time_out_seconds: float = 1) -> bool:
+        """ Connects to the server. Timeouts after time_out_seconds.
+        Returns true if a connection was established, otherwise returns false.
+        """
+        try:
+            self.client = socket.create_connection((server_ip, server_port), time_out_seconds)
+            return True
+        except TimeoutError as e:
+            print("Failed to established a connection: " + str(e))
+            return False
+
+    def send(self, message: str):
+        """ Formats the message string as ASCII and sends it to the server.
+        """
+        message = message.encode('ascii')
+        print("Trying to send message: ", message)
+        data = bytearray(message)
+        # data.append(self.end_of_message as byte)
+        self.client.sendall(data)
+        self.client.send(self.end_of_message)
+
+    def get_message(self):
+        """ This function will block until a message has been received
+        """
+        # The server will send 4 bytes of data containing the length of the next message
+        length_of_message = 4
+        data_buffer = bytearray()
+        while len(data_buffer) < length_of_message:
+            data_buffer += self.client.recv(length_of_message - len(data_buffer))
+        
+        length_of_message = 0
+        for byte in data_buffer:
+            length_of_message = (length_of_message << 8) + byte
+        
+        data_buffer = bytearray()
+        while len(data_buffer) < length_of_message:
+            data_buffer += self.client.recv(length_of_message - len(data_buffer))
+
+        return data_buffer.decode()
+    
+    def get_message(self):
+        """ This function will block until a message has been received
+        """
+        # The server will send 4 bytes of data containing the length of the next message
+        length_of_message = 4
+        data_buffer = bytearray()
+        while len(data_buffer) < length_of_message:
+            data_buffer += self.client.recv(length_of_message - len(data_buffer))
+        
+        length_of_message = 0
+        for byte in data_buffer:
+            length_of_message = (length_of_message << 8) + byte
+        
+        data_buffer = bytearray()
+        while len(data_buffer) < length_of_message:
+            data_buffer += self.client.recv(length_of_message - len(data_buffer))
+
+        return data_buffer
+
+
+
+    def setup_connection(self, user_id: int, name: str, server_ip: str, server_port: int, 
+                         time_out_seconds: float = 10):
+        result = self.connect(server_ip, server_port, time_out_seconds)
+        if result:
+            message = f'REGISTER:user_id={user_id},name={name},big_endian=1'
+            print("Sending message: ", message)
+            self.send(message)
+            # message = self.client.recv(4096) # Wait for "REGISTER" signal from server
+            # m2 = self.client.recv(0)
+            confirmation_message = self.get_message() # Get confirmation.
+            if confirmation_message == "NOK":
+                print("Failed to connect")
+                result = False
+            else:
+                print(confirmation_message.decode())
+            # time.sleep(0.01)
+        
+        return result
+    
+        def init_game():
+            pass
+
+
+if __name__ == '__main__':
+    server_ip = "192.168.50.162"
+    server_port = 9080
+
+    user_id = 15
+    name = "Rasmus"
+
+    clients = []
+    start_time = datetime.datetime.now()
+    for x in range(4):
+        client = TCPClient(user_id) # TODO Fix this init to not need user_id
+        user_name = name + "-" + str(user_id)
+        client.setup_connection(user_id, user_name, server_ip, server_port)
+        clients.append(client)
+        user_id += 100
+        # print(client.get_message())
+    print("Time to complete all requests: ", datetime.datetime.now() - start_time)
+
+    for client in clients:
+        print(client.get_message().decode())
+    
+    for client in clients:
+        data = [x for x in client.get_message()]
+        print(data)
+
+
+    # clients.pop(1)
+    # time.sleep(1)
+
+    # user_id = 115
+
+    # client = TCPClient(user_id) # TODO Fix this init to not need user_id
+    # user_name = name + "-" + str(user_id)
+    # client.setup_connection(user_id, user_name, server_ip, server_port)
+    # clients.append(client)
+    print("Enter infinite loop")
+    while True:
+        pass
+
