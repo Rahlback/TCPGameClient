5d3f4b339368c1de5644f9396fdabca9899776de
Rahlback <14808903+Rahlback@users.noreply.github.com>
2024-04-27 15:52:56 +0200

    Small cleanup

--- rust/src/client/tcp_client.rs
+++ rust/src/client/tcp_client.rs
@@ -22,18 +22,11 @@ impl TCPClient {
             offset -= 8;
 
         }
-        println!("Message length: {}", message_length);
 
         let mut buffer_t: [u8; 1000000] = [0; 1000000];
         let range = usize::try_from(message_length).unwrap();
         let full_read_res = self.stream.read_exact(&mut buffer_t[0..range]);
 
-        // let s = from_utf8(&buffer_t[0..range]).unwrap();
-
-        // println!("Received: {:?} = {}", buffer_l, message_length);
-        // println!("  Message: {:?}", s);
-
-        // self.stream.read_to_string(buf)
         if res.is_err() {
             println!("TCPClient: Something went wrong! {}", res.err().unwrap().to_string());
         }
@@ -60,16 +53,10 @@ impl TCPClient {
     }
 
     pub fn send_message(&mut self, message: &String) -> Result<(), std::io::Error> {
-        // println!("Sending: {} <-- {:?}", &message, message.as_bytes());
-        // self.stream.write(buf)
-        // let flush_res = self.stream.flush();
-        let res = self.stream.write_all(message.as_bytes());
-
-        
-        return res;
+        self.stream.write_all(message.as_bytes())
     }
 
-    pub fn set_no_delay(&mut self, val: bool) {
+    pub fn _set_no_delay(&mut self, val: bool) {
         self.stream.set_nodelay(val).expect("set_nodelay call failed");
     }
 }
